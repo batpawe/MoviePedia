22141212f357991240fd84f9b5314ce813ab39a5
ChePsik <batpawe@gmail.com>
2020-04-28 13:11:48 +0200

    minor bugfixes

--- src/movies/views.py
+++ src/movies/views.py
@@ -27,10 +27,20 @@ def index(request):
 def result(request, movieTitle):
     ia = IMDb('http')
     movieQuery = ia.search_movie(movieTitle)[0]
-
+    print(ia.get_movie_infoset())
+    ia.update(movieQuery, ['release'])
+    #work checkpoint/stop here
     context = {
         'title': movieQuery.get('title'),
-        'cover': modUrl(movieQuery['cover url'])
-
+        'cover': modUrl(movieQuery['cover url']),
+        'release': movieQuery.get('release info'),
+        'awards': movieQuery.get('awards'),
+        'credits': movieQuery.get('full credits'),
+        'keywords': movieQuery.get('keywords'),
+        'plot': movieQuery.get('plot'),
+        'news': movieQuery.get('news'),
+        'sites': movieQuery.get('official sites'),
+        'video': movieQuery.get('video clips'),
+        'rating': movieQuery.get('vote details')
     }
     return render(request, 'result.html', context=context)
